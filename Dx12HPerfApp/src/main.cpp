535787b83552cb826a2584eb465337b6d501ca1c
jamescheng40 <cheng-james@hotmail.com>
2019-12-20 13:51:13 -0500

    creating first application and basic shader

---
+++ Dx12HPerfApp/src/main.cpp
@@ -0,0 +1,17 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <Shlwapi.h>
+
+#include "Dx12HPerfApp.h"
+
+#include <iostream>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+    
+    std::cout << "hello this is my first application" << std::endl;
+
+    return 0;
+}
+
---
+++ GenerateProjectFiles.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+PUSHD %~dp0
+
+SET CMAKE_GENERATOR="Visual Studio 16 2019"
+SET CMAKE_BINARY_DIR=build
+
+MKDIR %CMAKE_BINARY_DIR% 2>NUL
+PUSHD %CMAKE_BINARY_DIR%
+
+cmake -G %CMAKE_GENERATOR% -Wno-dev "%~dp0"
+
+IF ERRORLEVEL 1 (
+    PAUSE
+) ELSE (
+    START DirectX12HighPerformance.sln
+)
+
+POPD
+POPD
